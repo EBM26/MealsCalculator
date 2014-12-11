889f81495d6dbc469e1e97ebb40817b45af8b630
BHCR Loaner <bhcrloaner01@BHCRs-MacBook-Air.local>
2014-12-10 22:16:55 -0800

    finished calculator

---
+++ Calcualtor/app.js
@@ -0,0 +1,39 @@
+window.onload= function () {
+	var button = document.getElementById("calculate");
+	
+	
+	
+
+	button.onclick = function() {
+		var homeMeal = document.getElementById("AMCH").value;
+		var homeMealMonthly = homeMeal * 31;
+		document.getElementById("MCEH").innerHTML = homeMealMonthly + " $ ";
+		var resMeal = document.getElementById("AMR").value;
+		var resMealMonthly = resMeal * 31;
+		document.getElementById("MCER").innerHTML = resMealMonthly + " $ ";
+		var totalSavings = (resMealMonthly - homeMealMonthly);
+		document.getElementById("Savings").innerHTML = totalSavings + " $ ";
+		}
+	
+};
+
+
+
+
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
