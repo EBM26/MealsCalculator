17ec5844fba0996e8a9c6a637a3719fd800dacc1
aaronzdavis <aaronzdavis@gmail.com>
2014-12-11 10:31:04 -0800

    Make it AngularAwesome

--- Calcualtor/app.js
+++ Calcualtor/app.js
@@ -1,39 +1,17 @@
-window.onload= function () {
-	var button = document.getElementById("calculate");
-	
-	
-	
+// window.onload= function () {
 
-	button.onclick = function() {
-		var homeMeal = document.getElementById("AMCH").value;
-		var homeMealMonthly = homeMeal * 31;
-		document.getElementById("MCEH").innerHTML = homeMealMonthly + " $ ";
-		var resMeal = document.getElementById("AMR").value;
-		var resMealMonthly = resMeal * 31;
-		document.getElementById("MCER").innerHTML = resMealMonthly + " $ ";
-		var totalSavings = (resMealMonthly - homeMealMonthly);
-		document.getElementById("Savings").innerHTML = totalSavings + " $ ";
-		}
-	
-};
+// 	var button = document.getElementById("calculate");
 
+// 	button.onclick = function() {
+// 		var homeMeal = document.getElementById("AMCH").value;
+// 		var homeMealMonthly = homeMeal * 31;
+// 		document.getElementById("MCEH").innerHTML = homeMealMonthly + " $ ";
+// 		var resMeal = document.getElementById("AMR").value;
+// 		var resMealMonthly = resMeal * 31;
+// 		document.getElementById("MCER").innerHTML = resMealMonthly + " $ ";
+// 		var totalSavings = (resMealMonthly - homeMealMonthly);
+// 		document.getElementById("Savings").innerHTML = totalSavings + " $ ";
+// 	}
 
-
-
- 
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// };
 
